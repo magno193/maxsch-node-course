180375686d4ff49bb2de9ac5aeecf3fcdb211beb
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-25 12:40:40 -0300

    Utilização de body-parser e form para req.body

--- 03-express-basics/app.js
+++ 03-express-basics/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use('add-product', (req, res, next) => {
-  res.send('<h1>Página de adição de produto</h1>')
+let step = 0;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/add-product', (req, res, next) => {
+  console.log('🛒', step++);
+  res.send(`
+  <form action="product" method="POST">
+    <input type="text" name="title">
+    <button type="submit">Adicionar produto</button>
+  </form>
+  `);
+});
+
+app.post('/product', (req, res) => {
+  console.log('🛒', step++, req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
+  console.log('🤩', step++);
   res.send('<h1>Hello from express</h1>')
 });
 
